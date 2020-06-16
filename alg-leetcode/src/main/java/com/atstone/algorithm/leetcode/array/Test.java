72dd796cbd6e9a5b8946c9a3761bb73c4acf0f6c
ssi-zhangc@dfmc.com.cn <12345678>
2020-06-16 20:38:51 +0800

    leetcode:88:删除链表重复元素

--- alg-leetcode/src/main/java/com/atstone/algorithm/leetcode/array/Test.java
+++ alg-leetcode/src/main/java/com/atstone/algorithm/leetcode/array/Test.java
@@ -2,6 +2,32 @@ package com.atstone.algorithm.leetcode.array;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("just have a test !");
+    }
+
+    /**
+     * 83. 删除排序链表中的重复元素
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (pre != null && cur != null){
+            if(pre.val == cur.val){
+                pre.next = cur.next;
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
